48ad84f53a0f7ab4a31cd5fc4e3419cec9b72613
AlexKuro <115189188+AlexKuro@users.noreply.github.com>
2023-01-31 23:36:27 +0300

    Добавлены задачи 1, 2, 3

---
+++ task_3.py
@@ -0,0 +1,23 @@
+import random
+
+""" Задача 3. Задайте список из n чисел последовательности
+(1 + 1/n)^n и выведите на экран их сумму. """
+
+
+print("\nПрограмма, выводит сумму последовательности (1 + 1/n)^n")
+# number = int(input("Введите число, для набора произведений чисел: "))
+number = random.randint(3, 10)
+result = 0
+sum = 0
+
+print(f"Последовательность (1 + 1/n)^n из {number} чисел--> ", end='[ ')
+
+for i in range(1, number + 1):
+    result = round(((1 + 1/i)**i), 2)
+    sum += result
+    print(result, end=' ')
+
+print(end=']')
+print(f"\nСумма последовательности (1 + 1/n)^n из {number} чисел--> {sum}")
+
+print(end='\n\n')
