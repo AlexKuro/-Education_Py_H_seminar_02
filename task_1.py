f039efb8ea0c4c9f37cea1c93723074aac41007c
AlexKuro <115189188+AlexKuro@users.noreply.github.com>
2023-02-02 21:07:13 +0300

    Исправлены ошибки
    Теперь можно вводить числа типа int, float, а так же отрицательные числа.

--- task_1.py
+++ task_1.py
@@ -6,16 +6,24 @@
 6782 -> 23
 0,56 -> 11 
 """
-print("\nЯ подсчитаю сумму всех введеных цифр.")
-numbStr = input("Введите любое число, тип int или float: ")
 
+print("\nЯ подсчитаю сумму всех введеных цифр.")
+
+numbStr = input("Введите любое число, тип int или float: ")
 resultSum = 0
-if float(numbStr) < 1:
-    for i in range(2, len(numbStr)):
-        resultSum += int(numbStr[i])
-elif float(numbStr) > 0:
-    for i in range(0, len(numbStr)):
-        resultSum += int(numbStr[i])
+numList = []
+
+for i in range(0, len(numbStr)):
+    numList.append(numbStr[i])
+
+for i in numList:
+    if i == '-':
+        numList.remove('-')
+    elif i == '.':
+        numList.remove('.')
+
+for i in numList:
+    resultSum += int(i)
 
 print("\tСумма цифр", end=' ')
 print(f"{numbStr} --> {resultSum}\n")
